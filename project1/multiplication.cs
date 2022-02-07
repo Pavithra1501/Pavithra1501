c4d19b54cc3e909a3668d5c6fa961548ff0c178d
Pavithra1501 <99183068+Pavithra1501@users.noreply.github.com>
2022-02-07 22:45:55 +0530

    Add files via upload

---
+++ project1/multiplication.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project1
+{
+    class multiplication
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the number:");
+                int n = Convert.ToInt32(Console.ReadLine());
+            int i = 1;
+            while(i<=10)
+            {
+                int x = n * i;
+                Console.WriteLine(n+ "*" +i +"="+x);
+                i++;
+                Console.ReadLine();
+            }
+        }
+    }
+}
