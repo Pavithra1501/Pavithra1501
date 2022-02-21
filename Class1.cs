4629a4051cc1ac06df49e115dca656c8659ff47e
Pavithra1501 <99183068+Pavithra1501@users.noreply.github.com>
2022-02-21 15:32:53 +0530

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dresses
+{
+    public class Dress
+    {
+        public int DressId { get; set; }
+        public string DressColor { get; set; }
+        public string DressBrand  { get; set; }
+        public int  DressSize { get; set; }
+    }
+}
